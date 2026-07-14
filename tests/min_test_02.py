404ce01b8feba6a4ddef1b2db20fd100d78123fa
KatjaPopova <popovaea@tutu.ru>
2026-07-14 18:04:32 +0300

    test_form_04 corrected

---
+++ tests/min_test_02.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+
+
+def test_min_test_02():
+# 1. Запуск браузера Chrome
+    driver = webdriver.Chrome()
+# 2. Открытие страницы
+    driver.get("https://qa-guru.github.io/one-page-form/text-box.html")
+    driver.maximize_window()
+    time.sleep(5)  # Пауза, чтобы визуально заметить открытие
+    print("Тест успешно пройден!")
+# 3. Закрытие браузера в любом случае
+    driver.quit()
--- tests/test_form_04.py
+++ tests/test_form_04.py
@@ -66,7 +66,7 @@ def test02():
 
         # Находим поле Email по его ID и вводим текст
         email_field = driver.find_element(By.ID, "userEmail")
-        email_field.send_keys("ivanexample.com")
+        email_field.send_keys("ivane@xample.com")
 
         # Находим кнопку Submit по ее ID и кликаем
         submit_button = driver.find_element(By.ID, "submit")
@@ -75,16 +75,54 @@ def test02():
         # 4. Проверка результата
         time.sleep(5)  # Пауза, чтобы увидеть результат отправки
         
-        # Находим блок с отправленными данными
-        result_box = driver.find_element(By.ID, "output")
+        # Получаем текст из поля Name
+        name_result = driver.find_element(By.ID, "name").text
         
-        # Проверяем, что в блоке результата появился введенный текст
-        assert "ivanexample.com" in result_box.text
-        print("Тест успешно пройден!")
+        # Проверяем, что поле Name не заполнено
+        assert name_result == "Name:"
+
 
     finally:
         # 5. Закрытие браузера в любом случае
         driver.quit()
 
-test01()
-test02()
+
+def test03():
+    print("Рефакторинг - итерация 1!")
+
+    # 1. Запуск браузера Chrome
+    driver = webdriver.Chrome()
+
+    try:
+        # 2. Открытие страницы
+        driver.get("https://qa-guru.github.io/one-page-form/text-box.html")
+        driver.maximize_window()
+        time.sleep(5)  # Пауза, чтобы визуально заметить открытие
+
+        # 3. Поиск элементов и заполнение полей
+        # Находим поле Full Name по его ID и вводим текст
+        full_name_field = driver.find_element(By.ID, "userName")
+        # web_elements = driver.find_elements(By.XPATH, "someXPath")
+
+        full_name_field.send_keys("Иван Иванов")
+
+        # Находим поле Email по его ID и вводим текст
+        email_field = driver.find_element(By.ID, "userEmail")
+        email_field.send_keys("ivanexample.com")
+
+        # Находим кнопку Submit по ее ID и кликаем
+        submit_button = driver.find_element(By.ID, "submit")
+        submit_button.click()
+
+        # 4. Проверка результата
+        time.sleep(5)  # Пауза, чтобы увидеть результат отправки
+
+        # 5. Получаем текст ошибки
+        error_text = email_field.get_attribute("validationMessage")
+
+        # Проверяем, что ошибка есть
+        assert error_text != ""
+
+    finally:
+        # 5. Закрытие браузера в любом случае
+        driver.quit()
